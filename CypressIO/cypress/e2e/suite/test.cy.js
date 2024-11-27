627c5c83a34c92dfb551a21375651616187d1ea7
Fernando <poncefernandoandres@gmail.com>
2024-11-27 17:30:00 -0300

    US001 pass test

--- CypressIO/cypress/e2e/suite/test.cy.js
+++ CypressIO/cypress/e2e/suite/test.cy.js
@@ -1,13 +1,44 @@
+describe("🔬 US 001 - TS 001 - Redireccion - Acceso a la Página Principal de Administración de FastFood", () => {
+  it("US 001 - TS 001 - TC 001 - Validar, redireccionar a la Interfaz Principal de Administración, cuando se introduce la URL correspondiente", () => {
+    cy.viewport(1366, 641);
 
-describe('🔬 US 001 - TS 001 - Redireccion - Acceso a la Página Principal de Administración de FastFood', () => {
-
-  it('US 001 - TS 001 - TC 001 - Validar, redireccionar a la Interfaz Principal de Administración, cuando se introduce la URL correspondiente', () => {
-
-    cy.viewport(1920, 1080);
     cy.visit("http://desarrollowebecommerce.somee.com/");
-    cy.get("div.popup-buttons>#ContentPlaceHolder1_lbHome").should("be.visible");
-    cy.get("div.popup-buttons>#ContentPlaceHolder1_lbHome").click();
-    // cy.url().should("have.text", "/User/Default.aspx");
-    
-  })
-})
+
+    cy.fixture("DOM/Page").then((fromThe) => {
+
+      cy.get(fromThe.initialPage.link.home).should("be.visible").click();
+
+      cy.url().should("contain", "/User/Default.aspx");
+
+      cy.get(fromThe.headerPage.link.login)
+        .should("have.text", "Login")
+        .and("not.be.empty")
+        .click();
+
+      cy.url().should("contain", "/User/Login.aspx");
+
+      
+      cy.get(fromThe.loginPage.input.username)
+        .should("be.visible")
+        .click()
+        .type(fromThe.loginPage.data.usernameAdmin.valid);
+  
+      cy.get(fromThe.loginPage.input.password)
+        .should("be.visible")
+        .click()
+        .type(fromThe.loginPage.data.passwordAdmin.valid);
+
+        cy.get(fromThe.loginPage.button.login)
+          .should("be.visible")
+          .should("be.enabled")
+          .click({ force: true });
+
+          cy.url().should("contain", "/Admin/Dashboard.aspx");
+      
+          cy.get(fromThe.dashboardPage.breadcrumb).should(
+            "be.visible"
+          );
+    });
+
+  });
+});
