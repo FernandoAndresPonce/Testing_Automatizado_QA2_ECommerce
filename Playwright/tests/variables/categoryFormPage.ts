f521996ddbe5970abea2067621f325c590e50459
Fernando <poncefernandoandres@gmail.com>
2024-12-09 13:05:14 -0300

    add variables dinamic, category Form Page

--- Playwright/tests/variables/categoryFormPage.ts
+++ Playwright/tests/variables/categoryFormPage.ts
@@ -1,11 +1,13 @@
 
 export const generateRandomWord = (length: number) => {
+    
     const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
     let randomWord = '';
 
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
-        randomWord += characters[randomIndex]; 
+        randomWord += characters[randomIndex];
     }
 
     return randomWord;
@@ -13,9 +15,9 @@ export const generateRandomWord = (length: number) => {
 
 
 export const validRandomCategoryName = () => {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';  
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    const minLength = 1;  
+    const minLength = 1;
     const maxLength = 50;
 
     const length = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
@@ -29,3 +31,28 @@ export const validRandomCategoryName = () => {
 
     return randomWord;
 };
+
+export const validRandomCategoryName1Character = () => {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const randomIndex = Math.floor(Math.random() * characters.length);
+
+    let randomWord = characters[randomIndex];
+
+    return randomWord;
+};
+
+export const validRandomCategoryName50Character = () => {
+
+    const character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const maxLength = 50;
+    let randomWord = "";
+
+    for (let i = 0; i < maxLength; i++) {
+        const randomIndex = Math.floor(Math.random() * character.length);
+        randomWord += character[randomIndex];
+    }
+
+    return randomWord;
+}
