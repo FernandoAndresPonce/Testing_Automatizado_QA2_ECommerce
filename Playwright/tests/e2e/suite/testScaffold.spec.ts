5579f4170ecfef73690adf55b5efc385ec91803b
Fernando <poncefernandoandres@gmail.com>
2025-02-04 20:52:23 -0300

    US015, add new test case, i found several bug in this case.

--- Playwright/tests/e2e/suite/testScaffold.spec.ts
+++ Playwright/tests/e2e/suite/testScaffold.spec.ts
@@ -18,7 +18,9 @@ dotenv.config();
 
 test.describe("🔬 US 015 - TS 015 - Botón Edit - Acceso a la Interfaz de “Edit Category” desde “Detalle de la Categoría”.", async () => {
 
-    test.beforeEach("🔲 BACKGROUND:", async ({ page, categoryPage, categoryDetail }) => {
+    let idCategory: string;
+
+    test.beforeEach("🔲 BACKGROUND:", async ({ page, categoryPage, categoryDetail, adminPage }) => {
         await test.step("📝 GIVEN que el Usuario esta Logeado como Admin -  ha pasado por un proceso de autenticación y autorizacion, es decir, ha iniciado sesión con credenciales con rol Administrador", async () => {
 
             await page.goto("/");
@@ -26,28 +28,30 @@ test.describe("🔬 US 015 - TS 015 - Botón Edit - Acceso a la Interfaz de “E
         });
 
         await test.step(" AND el admin ha seleccionado una Categoría “x” de dicha tabla,", async () => {
-            
+
             await expect(categoryPage.$table, "La tabla NO esta Visible").toBeVisible();
             await expect(categoryPage.$table, "La tabla NO esta Disponible").toBeEnabled();
 
             const rows = await categoryPage.$tableRows.all();
-            const lenghtRows : number = await rows.length;
-            
+            const lenghtRows: number = await rows.length;
+
             // console.log("Cantidad Filas: " + lenghtRows);
             await expect(lenghtRows).toBeGreaterThan(1);
 
             const randomIndex = Math.floor(Math.random() * (lenghtRows - 1) + 1);
 
-            await expect (categoryPage.$tableRows.nth(randomIndex), `La fila ${randomIndex} NO esta Visible.`).toBeVisible();
-            await expect (categoryPage.$tableRows.nth(randomIndex), `La fila ${randomIndex} NO esta Disponible.`).toBeEnabled();
+            await expect(categoryPage.$tableRows.nth(randomIndex), `La fila ${randomIndex} NO esta Visible.`).toBeVisible();
+            await expect(categoryPage.$tableRows.nth(randomIndex), `La fila ${randomIndex} NO esta Disponible.`).toBeEnabled();
 
-            //seguir por aqui 
-            const idCategory = await categoryDetail.$idCategoryLabel.innerText();
-            console.log(idCategory);
-            
             await categoryPage._clickEyeRowButton(randomIndex);
-            
+
             await expect(categoryDetail.$viewCategoryTitle, "El Titulo View Category NO esta Visible.").toBeVisible();
+
+            await adminPage._hiddenLoader();
+
+            //seguir por aqui.
+            idCategory = await categoryDetail.$idCategoryLabel.innerText();
+            console.log("ID CATEGORY:" + idCategory);
         });
     });
 
@@ -58,7 +62,7 @@ test.describe("🔬 US 015 - TS 015 - Botón Edit - Acceso a la Interfaz de “E
             await expect(categoryDetail.$editButton, "El Button NO es Visible.").toBeVisible();
             await expect(categoryDetail.$editButton, "El Button NO esta Disponible.").toBeEnabled();
 
-            await categoryDetail._clickEditButton();     
+            await categoryDetail._clickEditButton();
         });
 
         await test.step(" THEN el sistema se deberia redirecciona a la Interfaz de “Edit Category", async () => {
@@ -69,13 +73,81 @@ test.describe("🔬 US 015 - TS 015 - Botón Edit - Acceso a la Interfaz de “E
         });
     })
 
-    test("US 015 - TS 015 - TC 001 - Intentar Validar, redireccionar a la Interfaz “Edit Category”, al ingresar la url con el ID de la categoria.", async ({page}) => {
+    test("US 015 - TS 015 - TC 002 - Intentar Validar, redireccionar a la Interfaz “Edit Category”, al ingresar la url con el ID de una categoria.", async ({ page, categoryFormPage, editCategory }) => {
 
         await test.step("WHEN hace ingresa la URL en el ID de la Categoria,", async () => {
 
+
+            await page.goto(categoryFormPage.endpoint + "?Id=" + idCategory)
         });
 
         await test.step("THEN el sistema se deberia redirecciona a la Interfaz de “Edit Category", async () => {
+
+            await expect(page).toHaveURL(categoryFormPage.endpoint + "?Id=" + idCategory);
+
+            await expect(editCategory.$editCategoryTitle, "El titulo Edit Category NO es Visible.").toBeVisible();
+            await expect(editCategory.$editCategoryTitle, "El titulo NO es Edit Category.").toHaveText("Edit Category");
+        });
+    });
+
+    test("US 015 - TS 015 - TC 003 - Intentar Validar, redireccionar a la Interfaz “Categories” como /Admin/Category.aspx, al ingresar la url con el ID de una categoria que NO Existe.", async ({ page, categoryFormPage, editCategory }) => {
+
+        await test.step("WHEN hace ingresa la URL en el ID de la Categoria,", async () => {
+
+
+            await page.goto(categoryFormPage.endpoint + "?Id=" + 0)
+        });
+
+        await test.step("THEN el sistema se deberia redirecciona a la Interfaz de “Edit Category", async () => {
+
+
+            await expect(page).toHaveURL("/Admin/Error.aspx");
+
+        });
+    });
+
+    test("US 015 - TS 015 - TC 004 - Intentar Validar, redireccionar a la Interfaz “Categories” como /Admin/Category.aspx, al ingresar la url con un ID Vacio.", async ({ page, categoryFormPage, editCategory }) => {
+
+        await test.step("WHEN hace ingresa la URL en el ID de la Categoria,", async () => {
+
+
+            await page.goto(categoryFormPage.endpoint + "?Id=")
+        });
+
+        await test.step("THEN el sistema se deberia redirecciona a la Interfaz de “Edit Category", async () => {
+
+            await expect(page).toHaveURL("/Admin/Error.aspx");
+
+        });
+    });
+
+
+    test("US 015 - TS 015 - TC 005 - Intentar Validar, redireccionar a la Interfaz “Categories” como /Admin/Category.aspx, al ingresar la url con un ID Alfabetico.", async ({ page, categoryFormPage, editCategory }) => {
+
+        await test.step("WHEN hace ingresa la URL en el ID de la Categoria,", async () => {
+
+
+            await page.goto(categoryFormPage.endpoint + "?Id=A")
+        });
+
+        await test.step("THEN el sistema se deberia redirecciona a la Interfaz de “Edit Category", async () => {
+
+            await expect(page).toHaveURL("/Admin/Error.aspx");
+
+        });
+    });
+
+    test("US 015 - TS 015 - TC 006 - Intentar Validar, redireccionar a la Interfaz “Categories” como /Admin/Category.aspx, al ingresar la url con un ID Alfabetico.", async ({ page, categoryFormPage, editCategory }) => {
+
+        await test.step("WHEN hace ingresa la URL en el ID de la Categoria,", async () => {
+
+
+            await page.goto(categoryFormPage.endpoint + "?Id=$")
+        });
+
+        await test.step("THEN el sistema se deberia redirecciona a la Interfaz de “Edit Category", async () => {
+
+            await expect(page).toHaveURL("/Admin/Error.aspx");
 
         });
     });
